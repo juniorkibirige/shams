1f5e56ca9a55153d6991aabce8e91c60ae0a0dcb
Kibirige Junior Lawrence <aanga2@gmail.com>
2020-04-13 21:54:54 +0300

    Initial Commit

---
+++ matrix.php
@@ -0,0 +1,17 @@
+<?php
+if($_GET['key'] && $_GET['origins'] && $_GET['destinations']){
+    // echo json_encode($_GET);
+    $key = $_GET['key'];
+    $o = $_GET['origins'];
+    $d = $_GET['destinations'];
+    $data = 'origins='.$o.'&destinations='.$d.'&key='.$key;
+    $url = 
+    'https://api.distancematrix.ai/maps/api/distancematrix/json?' . $data;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $result = curl_exec($ch);
+    curl_close(($ch));
+    echo $result;
+}
+?>
