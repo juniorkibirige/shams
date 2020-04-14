8988b2c272c6815243f6dd5375e107e2d3da7c58
Kibirige Junior Lawrence <aanga2@gmail.com>
2020-04-14 05:15:22 +0300

    Corrected MongoDB URI

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -13,7 +13,7 @@ const options = {
 
 const connectWithRetry = _ => {
     console.log('MongoDB connection with retry')
-    mongoose.connect("mongodb://localhost:27017/api-shamserrand", options).then( _ => {
+    mongoose.connect("mongodb://heroku_b8gxlx07:41f10v1944kvoujk11nd1sks06@ds019746.mlab.com:19746/heroku_b8gxlx07", options).then( _ => {
         console.log('MongoDB is connected')
     }) .catch(err => {
         console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', ++count)
