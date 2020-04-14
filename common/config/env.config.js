f7e021dc9fac1729435795fc78184572fa3e3828
Kibirige Junior Lawrence <aanga2@gmail.com>
2020-04-14 05:31:22 +0300

    improvising

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     "port": 3000,
-    "appEndpoint": 'https://api-shamserrand.herokuapp.com:3000',
-    "apiEndpoint": 'https://api-shamserrand.herokuapp.com:3000',
+    "appEndpoint": 'https://api-shamserrand.herokuapp.com',
+    "apiEndpoint": 'https://api-shamserrand.herokuapp.com',
     "jwt_secret": "s3CuR3P45sw0R6",
     "jwt_expiration_in_seconds": 36000,
-    "environment": "prod",
+    "environment": "dev",
     "permissionLevels": {
         "NORMAL_USER": 1,
         "PAID_USER": 4,
